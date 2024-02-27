f64a68ea20bdb6ff795eeb45b9edc733a32cdc64
R2D2-like <tsukurobokun@gmail.com>
2024-02-27 23:57:53 +0900

    add wiping cylinder gripper

--- robosuite/models/grippers/__init__.py
+++ robosuite/models/grippers/__init__.py
@@ -11,6 +11,7 @@ from .jaco_three_finger_gripper import JacoThreeFingerGripper, JacoThreeFingerDe
 from .robotiq_140_gripper import Robotiq140Gripper
 from .wiping_gripper import WipingGripper
 from .null_gripper import NullGripper
+from .wiping_cylinder_gripper import WipingCylinderGripper
 
 
 GRIPPER_MAPPING = {
@@ -23,6 +24,7 @@ GRIPPER_MAPPING = {
     "Robotiq140Gripper": Robotiq140Gripper,
     "RobotiqThreeFingerGripper": RobotiqThreeFingerGripper,
     "RobotiqThreeFingerDexterousGripper": RobotiqThreeFingerDexterousGripper,
+    "WipingCylinderGripper": WipingCylinderGripper,
     None: NullGripper,
 }
 
